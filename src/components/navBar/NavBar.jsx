d0461680f4fea6f91c4e5c220cc2751366279265
ExpoJuy <crystaldesarrollo@gmail.com>
2023-03-08 18:43:34 -0300

    Se empezó a trbajar en menú responsive

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,24 +1,30 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 import "./navbar.css";
 
 function NavBar() {
   return (
     <div className="navBarContainer">
-        <div className="logo">
-            <p>LOGO</p>
-        </div>
-        <div className="navegacion">
-            <Link to={"/"}>Inicio</Link>
-            <Link to={"/"}>Cátedra</Link>
-            <Link to={"/"}>Noticias</Link>
-            <Link to={"/"}>Enlaces de Interes</Link>
-            <Link to={"/"}>Eventos</Link>
-            <Link to={"/"}>Biblioteca</Link>
-            <Link to={"/"}>Contacto</Link>
-        </div>
+      <div className="logo">
+        <p>LOGO</p>
+      </div>
+      <div className="navegacion">
+        <Link to={"/"}>Inicio</Link>
+        <Link to={"/"}>Cátedra</Link>
+        <Link to={"/"}>Noticias</Link>
+        <Link to={"/"}>Enlaces de Interes</Link>
+        <Link to={"/"}>Eventos</Link>
+        <Link to={"/"}>Biblioteca</Link>
+        <Link to={"/"}>Contacto</Link>
+        <Link to={"/"}>Iniciar Sesión</Link>
+      </div>
+      <div className="hamburguesa">
+        <div className="linea 1"></div>
+        <div className="linea 2"></div>
+        <div className="linea 3"></div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default NavBar;
--- src/components/sectionCrypto/SectionCrypto.jsx
+++ src/components/sectionCrypto/SectionCrypto.jsx
@@ -1,24 +1,37 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Calculadora from '../calculadora/Calculadora';
+import React from "react";
+import { Link } from "react-router-dom";
+import Calculadora from "../calculadora/Calculadora";
 import "./sectioncrypto.css";
 
 function SectionCrypto() {
   return (
     <div className="sectionCrypto">
-        <div className="ejemploIzquierda">
-            <h3>Pero con un ejemplo lo vamos a entender mejor.</h3>
-            <p>Creo que todos escuchamos el termino "crypto monedas" ¿verdad? ¿Sabías que el valor de <b>1 Bitcoin</b> es de más de <b>20.000 dolares</b>? Eso quiere decir que 1 Bitcoin equivale a más de <b>SIETE MILLONES DE PESOS</b>. Woow ya empezamos a notar como un número chico se transforma en uno grande. Pero, ¿que hay de las posiciones decimales? Claro que tienen un valor y por eso vamos usar esta calculadora en tiempo real para demostrar que toda posición decimal tiene un valor, y como un número en una posición decimal "lejana" puede significar una gran diferencia en nuestro bolsillo.</p>
-            <Link to={"/unidades"}>Ir a las unidades</Link>
-        </div>
-        <div className="ejemploDerecha">
-            <h3>Ingresa un valor con decimales y mira la magia</h3>
-            <Calculadora/>
-            <p>Aquí podemos ver como un número decimal muy pequeño, puede hacer una GRAN diferencia dependiendo del <b>contexto</b></p>
-            <p>Para no complicarnos, supongamos que 1 Bitcoin = 7 millones de pesos</p>
-        </div>
+      <div className="ejemploIzquierda">
+        <h3>Pero con un ejemplo lo vamos a entender mejor.</h3>
+        <p>
+          Creo que todos escuchamos el termino "crypto monedas" ¿verdad? ¿Sabías
+          que el valor de <b>1 Bitcoin</b> es de más de <b>20.000 dolares</b>?
+          Eso quiere decir que 1 Bitcoin equivale a más de{" "}
+          <b>SIETE MILLONES DE PESOS</b>. Woow ya empezamos a notar como un
+          número chico se transforma en uno grande. Pero, ¿que hay de las
+          posiciones decimales? Claro que tienen un valor y por eso vamos usar
+          esta calculadora en tiempo real para demostrar que toda posición
+          decimal tiene un valor, y como un número en una posición decimal
+          "lejana" puede significar una gran diferencia en nuestro bolsillo.
+        </p>
+        <Link to={"/unidades"}>Ir a las unidades</Link>
+      </div>
+      <div className="ejemploDerecha">
+        <h3>Ingresa un valor con decimales y mira la magia</h3>
+        <img src="./img/bitcoin.png" alt="imagen bitcoin" />
+        <p>
+          Aquí podemos ver como un número decimal muy pequeño, puede hacer una
+          GRAN diferencia dependiendo del <b>contexto</b>
+        </p>
+        <Calculadora />
+      </div>
     </div>
-  )
+  );
 }
 
 export default SectionCrypto;
