63530ddac68d42a3b4d2248798c3d7bcec35274d
ExpoJuy <crystaldesarrollo@gmail.com>
2023-03-13 19:58:04 -0300

    Seccion de contacto responsiva terminada, falta agregar funcionalidad js

--- src/components/sectionContacto/SectionContacto.jsx
+++ src/components/sectionContacto/SectionContacto.jsx
@@ -1,14 +1,35 @@
 import React from "react";
+import { faVideo, faPlay } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./sectioncontacto.css";
 
 function SectionContacto() {
+  const logoYouTube = <FontAwesomeIcon icon={faPlay} />;
   return (
     <div id="contacto" className="sectionContacto">
       <h3>Contacto</h3>
-      <form className="contactocointainer">
-        <div className="contactoIzquierda"></div>
-        <div className="contactoDrecha"></div>
-        <div className="lineaInferior"></div>
+      <h4>Dejanos tu consulta</h4>
+      <form className="contactoContainer">
+        <div className="lineaSuperior">
+          <div className="contactoIzquierda">
+            <input type="text" placeholder="Nombre Completo" />
+            <input type="email" placeholder="Correo electrónico" />
+            <input type="number" placeholder="Telefono de contacto" />
+          </div>
+          <div className="contactoDerecha">
+            <textarea name="" id="" placeholder="Tu consulta"></textarea>
+          </div>
+        </div>
+        <div className="lineaInferior">
+          <button>Enviar</button>
+          <a
+            href="https://www.youtube.com/@victoraizama1068/videos"
+            target="_blank"
+          >
+            {logoYouTube}
+            <p>YouTube</p>
+          </a>
+        </div>
       </form>
     </div>
   );
